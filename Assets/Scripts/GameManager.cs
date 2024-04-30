e4b1fc30d2eb5fc8e704e08efcd08672dfaa4a72
Dyllan Hope <dyllanjhope@gmail.com>
2024-04-30 06:49:01 +0100

    Linked the score and enemy count to UI
    Linked the Current score and the enemy count to the UI in order to display the current enemies remaining and the current score.
    
    Added points to healthManager for enemies.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI enemyCountText;
+
     int enemyCount = 0;
 
     int currentScore = 0;
@@ -16,16 +20,19 @@ public class GameManager : MonoBehaviour
     public void SetEnemyCount(int count)
     {
         enemyCount = count;
+        enemyCountText.text = "Enemies: " + enemyCount.ToString("000");
     }
 
     public void KillEnemy()
     {
-        enemyCount --;
+        enemyCount--;
+        enemyCountText.text = "Enemies: " + enemyCount.ToString("000");
     }
 
     public void IncreaseScore(int points)
     {
         currentScore += points;
+        scoreText.text = "Score: " + currentScore.ToString("0000");
     }
 
     public int GetCurrentScore()
